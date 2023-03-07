58c0c9bd0632ab97ce196792afdb4d50f80632c7
Peter Baumgartner <pete@lincolnloop.com>
2023-03-07 16:17:40 -0700

    all in one deploy action
    includes:
    * upload artifacts
    * trigger build
    * push images

--- index.js
+++ index.js
@@ -1,46 +1,86 @@
 const core = require("@actions/core");
-const exec = require("@actions/exec");
 const github = require("@actions/github");
-const tc = require("@actions/tool-cache");
-const yaml = require("js-yaml");
-const {
-  CodeBuildClient,
-  BatchGetProjectsCommand,
-  StartBuildCommand,
-} = require("@aws-sdk/client-codebuild");
-const {
-  ECRClient,
-  GetAuthorizationTokenCommand,
-} = require("@aws-sdk/client-ecr");
+const { exec } = require("@actions/exec");
+const fs = require('fs');
+const { downloadTool, extractTar } = require("@actions/tool-cache");
+const { load } = require("js-yaml");
+const { CodeBuildClient, BatchGetProjectsCommand, StartBuildCommand } = require("@aws-sdk/client-codebuild");
+const { ECRClient, GetAuthorizationTokenCommand } = require("@aws-sdk/client-ecr");
+const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
+
+const { startGroup, getInput, info, setOutput, warning, setFailed, endGroup } = core;
+const { context } = github;
+
 
 const craneDownloadPath =
-  "https://github.com/google/go-containerregistry/releases/download/v0.4.1/go-containerregistry_Linux_x86_64.tar.gz";
+  "https://github.com/google/go-containerregistry/releases/download/v0.13.0/go-containerregistry_Linux_x86_64.tar.gz";
+const codebuildImage = "public.ecr.aws/aws-cli/aws-cli:latest";
 
-async function main() {
-  if (process.platform !== "linux") {
-    core.setFailed("AppPack deploy can only run on Linux platforms");
-  }
-  if ("AWS_DEFAULT_REGION" in process.env && !("AWS_REGION" in process.env)) {
-    process.env.AWS_REGION = process.env.AWS_DEFAULT_REGION;
-  }
-  const codebuild = new CodeBuildClient();
-  let params = { names: [core.getInput("appname")] };
-  let command = new BatchGetProjectsCommand(params);
-  let data = await codebuild.send(command);
-  const project = data.projects[0];
-  const dockerRepo = project.environment.environmentVariables.find(
-    (e) => e.name === "DOCKER_REPO"
-  ).value;
-  const dockerRegistry = dockerRepo.split("/")[0];
-  const artifactsBucket = project.artifacts.location;
-  const buildArtifacts = yaml.load(project.source.buildspec).artifacts.files;
-  core.startGroup("Downloading crane");
-  const craneArchive = await tc.downloadTool(craneDownloadPath);
-  await tc.extractTar(craneArchive, "/tmp/crane");
-  core.endGroup();
+async function commitTxt() {
+  // exec `git log -n1` and save to `commit.txt`
+  startGroup("Writing commit.txt");
+  await exec("git", ["log", "-n1"], {
+    listeners: {
+      stdout: (data) => {
+        fs.writeFileSync("./commit.txt", data.toString());
+      },
+    },
+  });
+}
 
+async function codebuildProject() {
+  const codebuild = new CodeBuildClient({});
+  const params = { names: [getInput("appname")] };
+  const command = new BatchGetProjectsCommand(params);
+  const data = await codebuild.send(command);
+  return data.projects[0];
+}
+
+async function uploadArtifacts(artifactsBucket, files) {
+  info("Uploading artifacts");
+  const prefix = `external-${context.runNumber}/`;
+  setOutput("artifacts_bucket", artifactsBucket);
+  setOutput("artifacts_prefix", prefix);
+  const s3 = new S3Client({});
+  files.forEach((file) => {
+    fs.readFile(`./${file}`, "utf8", function (err, contents) {
+      if (err) {
+        if (err.code === "ENOENT") {
+          // this is a buildpack artifact which we don't expect BYOB to upload
+          if (file !== "metadata.toml") {
+            warning(
+              `${file} does not exist. It must be uploaded to S3 for a deploy to succeed.`
+            );
+          }
+        } else {
+          throw err;
+        }
+      } else {
+        const command = new PutObjectCommand({
+          Bucket: artifactsBucket,
+          Key: `${prefix}${file}`,
+          Body: contents,
+        });
+        info(`  * ${file}`);
+        s3.send(command).catch((error) => {
+          setFailed(error.message);
+        });
+      }
+    });
+  });
+  endGroup();
+}
+
+async function downloadCrane() {
+  startGroup("Downloading crane");
+  const craneArchive = await downloadTool(craneDownloadPath);
+  await extractTar(craneArchive, "/tmp/crane");
+  endGroup();
+}
+
+async function startBuild(artifactsBucket, buildArtifacts) {
   // trigger deploy via codebuild
-  core.startGroup("Triggering deploy");
+  startGroup("Triggering deploy");
   const buildspec = {
     version: 0.2,
     artifacts: {
@@ -49,38 +89,50 @@ async function main() {
     },
     phases: {
       build: {
-        commands: `aws s3 cp --recursive s3://${artifactsBucket}/external-${github.context.runNumber}/ .`,
+        commands: `aws s3 cp --recursive s3://${artifactsBucket}/external-${context.runNumber}/ .`,
       },
     },
   };
-  params = {
-    projectName: core.getInput("appname", { required: true }),
-    sourceVersion: github.context.ref,
+  const params = {
+    projectName: getInput("appname", { required: true }),
+    sourceVersion: context.ref,
     gitCloneDepthOverride: 1,
+    imageOverride: codebuildImage,
     buildspecOverride: JSON.stringify(buildspec),
   };
-  command = new StartBuildCommand(params);
-  data = await codebuild.send(command);
+  const codebuild = new CodeBuildClient({});
+  const command = new StartBuildCommand(params);
+  const data = await codebuild.send(command);
   const buildNumber = data.build.buildNumber;
-  core.info(`Started build #${buildNumber}`);
-  core.info(data.build.arn);
-  core.setOutput("build_number", buildNumber);
-  core.setOutput("build_arn", data.build.arn);
-  core.endGroup();
+  info(`Started build #${buildNumber}`);
+  info(data.build.arn);
+  setOutput("build_number", buildNumber);
+  setOutput("build_arn", data.build.arn);
+  endGroup();
+  return buildNumber;
+}
 
+async function pushImages(dockerRepo, buildNumber) {
   // tag docker image
-  core.startGroup(`Tagging image 'build-${buildNumber}'`);
-  const ecr = new ECRClient();
-  command = new GetAuthorizationTokenCommand({});
-  data = await ecr.send(command);
-  core.info("Logging into ECR repo");
+  startGroup(`Pushing images to ${dockerRepo}`);
+  const image = `${dockerRepo}:${context.sha}`;
+  await exec("docker", [
+    "tag",
+    getInput("image", { required: true }),
+    image,
+  ]);
+  const dockerRegistry = dockerRepo.split("/")[0];
+  const ecr = new ECRClient({});
+  const command = new GetAuthorizationTokenCommand({});
+  const data = await ecr.send(command);
+  info("Logging into ECR repo");
   const [username, password] = Buffer.from(
     data.authorizationData[0].authorizationToken,
     "base64"
   )
     .toString()
     .split(":");
-  await exec.exec(
+  await exec(
     '"/tmp/crane/crane"',
     [
       "auth",
@@ -94,17 +146,36 @@ async function main() {
       input: Buffer.from(password),
     }
   );
-  core.info("Tagging image");
-  await exec.exec('"/tmp/crane/crane"', [
-    "tag",
-    `${dockerRepo}:${github.context.sha}`,
-    `build-${buildNumber}`,
+  await exec("docker", ["push", image]);
+  info("Tagging image");
+  await exec('"/tmp/crane/crane"', ["tag", image, `build-${buildNumber}`]);
+  endGroup();
+}
+
+async function main() {
+  if (process.platform !== "linux") {
+    setFailed("AppPack deploy can only run on Linux platforms");
+  }
+  if ("AWS_DEFAULT_REGION" in process.env && !("AWS_REGION" in process.env)) {
+    process.env.AWS_REGION = process.env.AWS_DEFAULT_REGION;
+  }
+  const [project] = await Promise.all([
+    codebuildProject(),
+    commitTxt(),
+    downloadCrane(),
   ]);
-  core.endGroup();
+  const dockerRepo = project.environment.environmentVariables.find(
+    (e) => e.name === "DOCKER_REPO"
+  ).value;
+  const artifactsBucket = project.artifacts.location;
+  const buildArtifacts = load(project.source.buildspec).artifacts.files;
+  await uploadArtifacts(artifactsBucket, buildArtifacts);
+  const buildNumber = await startBuild(artifactsBucket, buildArtifacts);
+  await pushImages(dockerRepo, buildNumber);
 }
 
 if (require.main === module) {
   main().catch((error) => {
-    core.setFailed(error.message);
+    setFailed(error.message);
   });
 }
